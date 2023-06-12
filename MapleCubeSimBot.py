d7b7498af500e7c644082921793ae5edce261133
Hwi-sang Cho <hwisang@sas.upenn.edu>
2023-06-12 01:57:09 -0400

    second commit

---
+++ MapleCubeSimBot.py
@@ -0,0 +1,19 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+client.run(TOKEN)
