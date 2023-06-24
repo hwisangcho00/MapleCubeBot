7f68a114827d5bc22fbbab4a583c075e568dec8b
Hwi-sang Cho <hwisang@sas.upenn.edu>
2023-06-24 15:56:33 -0400

    Created CUbeSimulator.py Refactored how cubeProb.py holds the probability data Created cubeIdx.py for easier indexing

---
+++ cubeIdx.py
@@ -0,0 +1,8 @@
+cubeList = ["bright", "glowing"]
+rankList = ["rare", "epic", "unique", "legendary"]
+equipmentList = ["weapon", "emblem", "secondary", "hat", "eye", "face", "earings", "belt", "top", "bottom", "overall"
+                    "shoes", "gloves", "shoulder", "cape"]
+
+
+rankIdx = {v : i for i, v in enumerate(rankList)}
+equipmentIdx = {v : i for i, v in enumerate(equipmentList)}
