e54af36bda10898d01af0be809a834f660efb41a
Hwi-sang Cho <hwisang@sas.upenn.edu>
2023-06-25 16:53:48 -0400

    Used regex to match resLine with validCheckList. Update the validCount to keep track of past line results

--- CubeSimulator.py
+++ CubeSimulator.py
@@ -1,6 +1,7 @@
 import cubeProb
 import random
-from cubeIdx import equipmentIdx
+import re
+from cubeIdx import equipmentIdx, validCheckList
 
 
 class CubeSimulator:
@@ -10,14 +11,20 @@ class CubeSimulator:
         self.tier = tier
         self.equipment = equipment
         self.level = int(level)
+        self.validCount = [0] * len(validCheckList)
+        
 
     def rollCube(self) -> str:
         prob = 100
-        for lineNum in range(3):        
+        resLine = None
+        for lineNum in range(3):
+            print(self.validCount)
             probList = self.getProbList(lineNum)
+            if lineNum != 0:
+                self.changeProb(prob, probList, resLine)
             resLine = self.rollLine(probList, prob)
-            self.changeProb(prob, probList)
-            print(resLine)
+            self.updateValidCount(resLine)
+            print((lineNum + 1),'Line =',resLine)
 
 
     def rollLine(self, probList, prob) -> str:
@@ -55,7 +62,20 @@ class CubeSimulator:
             return
 
         if self.tier == 'legendary':
-            return cubeProb.legendary[equipmentIdx[self.equipment]][levelIdx][lineNum]
+            return cubeProb.legendary[equipmentIdx[self.equipment]][levelIdx][lineNum].copy()
         
-    def changeProb(self, prob, probList):
+    def changeProb(self, prob, probList, resLine):
         pass
+
+    def updateValidCount(self, resLine):
+        
+        for idx, potential in enumerate(validCheckList):
+            if re.match(potential, resLine):
+                self.validCount[idx] += 1
+                break
+        
+        return
+        
+
+
+        
--- cubeIdx.py
+++ cubeIdx.py
@@ -3,6 +3,12 @@ rankList = ["rare", "epic", "unique", "legendary"]
 equipmentList = ["weapon", "emblem", "secondary", "hat", "eye", "face", "earings", "belt", "top", "bottom", "overall"
                     "shoes", "gloves", "shoulder", "cape"]
 
+validCheckList = ["Enables the <Decent Haste> skill", "Enables the <Decent Combat Orders> skill", "Enables the <Decent Advanced Blessing> skill",
+                  "Enables the <Decent Mystic Door> skill", "Enables the <Decent Hyper Body> skill", 
+                  "Enables the <Decent Speed Infusion> skill", "Enables the <Decent Sharp Eyes> skill", 
+                  "Invincible for 3 more seconds after getting attacked", "Ignore Monster DEF : \+\d{0,2}",
+                  "\d{0,2}% chance to ignore \d{0,2}% damage when attacked", "\d{0,2}% chance to become invincible for \d{0,2} seconds",
+                  "Boss Monster Damage : \+\d{0,2}%", "Item Drop Rate : \+\d{0,2}%"]
 
 rankIdx = {v : i for i, v in enumerate(rankList)}
 equipmentIdx = {v : i for i, v in enumerate(equipmentList)}
