8e70a30a9f53552e5ebbae24dbbc1bcb63bdb9f3
Joseph Cho <hwisang@seas.upenn.edu>
2023-08-04 00:03:04 -0400

    create link to database Used CockroachDB and postgresql to keep track of a log to command

---
+++ DatabaseConnector.py
@@ -0,0 +1,52 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+class DatabaseConnector:
+    
+    def __init__(self):
+        load_dotenv()
+        self.DATABASE_URL = os.getenv('DATABASE_URL')
+
+    def incrementLog(self, name):
+        conn = psycopg2.connect(self.DATABASE_URL)
+        with conn.cursor() as cur:
+            try:
+              cur.execute("SET DATABASE = defaultdb")
+              cur.execute('''INSERT INTO log VALUES (DEFAULT, %s, current_timestamp);''', (name, ))
+              conn.commit()
+              cur.close()
+            except Exception as e:
+               print(e)
+            finally:
+              conn.close()
+  
+    def initiateDB(self):
+      conn = psycopg2.connect(self.DATABASE_URL)
+      with conn.cursor() as cur:
+          try:
+            cur.execute("SET DATABASE = defaultdb")
+            cur.execute('''CREATE TABLE IF NOT EXISTS log (
+                        ID SERIAL NOT NULL PRIMARY KEY, 
+                        Command VARCHAR(16),
+                        Time TIMESTAMP
+                        );''')
+            conn.commit()
+            cur.close()
+          except Exception as e:
+               print(e)
+          finally:
+            conn.close()
+
+    def testDB(self):
+        conn = psycopg2.connect(self.DATABASE_URL)
+        with conn.cursor() as cur:
+          try:
+            cur.execute("SET DATABASE = defaultdb")
+            cur.execute('''INSERT INTO log VALUES (DEFAULT, 'rank', current_timestamp);''')
+            conn.commit()
+            cur.close()
+          except Exception as e:
+               print(e)
+          finally:
+             conn.close()
